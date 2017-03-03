7c081ee71af5786bf911bea771c7c328c9faa693
rdottle <rachaelcdottle@gmail.com>
2017-03-02 22:03:34 -0500

    Add R script

--- TestingGround/Rscript/01_datacleaning.R
+++ TestingGround/Rscript/01_datacleaning.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(magrittr)
 library(tidyr)
 
-setwd("C://Users/racha/Desktop/Git/spring2017-datavis/02_Webpage/TestingGround/data/")
+setwd("C://Users/racha/Desktop/Git/webpage-dataviz/TestingGround/data/")
 colombian <- read.csv("03_Assignment_3_Data.csv")
 # 
 # head(colombian)
@@ -16,4 +16,8 @@ colombian_clean$age <- recode_factor(colombian_clean$age, 'entre 0 y 5' = '0 to 
                                      'entre 18 y 28' = '18 to 28 years', 'entre 29 y 60' = '29 to 60 years',
                                      'entre 6 y 11' = '6 to 11 years', 'entre 61 y 100' = '61 to 100 years')
 
+
 colombian_clean %>% write.csv('colombiandata.csv')
+
+colombian_clean2 <- read.csv("colombiandata.csv") %>% group_by(year) %>% mutate(value = value/sum(value)) %>%
+  write.csv('colombianclean2.csv')
